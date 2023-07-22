fbb9d93eb140fdbe871a262970e1f07773013d51
as-dodo <cestanastasia@gmail.com>
2023-07-22 19:09:08 -0300

    django migrations first

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def welcome(request):
     return HttpResponse("Welcome to the Meeting planner")
 
+def date(request):
+    return HttpResponse("this page was served at " +str(datetime.now()))
+
+def about(request):
+    return HttpResponse("Hello, this is a website that I'm trying to create using Django. By the way this is my dog's name")
